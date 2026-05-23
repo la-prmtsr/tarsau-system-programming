4a19f77c78c3568b7fd5403edb06764288ce0c23
MutiaKusuma <mutiamharani@gmail.com>
2026-05-23 13:55:39 +0300

    Implement basic SAU archive creation

--- include/tarsau.h
+++ include/tarsau.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 void createArchive(int fileCount, char *files[], char *outputName);
 void extractArchive(char *archiveName, char *directoryName);
--- src/archive.c
+++ src/archive.c
@@ -2,5 +2,89 @@
 
 void createArchive(int fileCount, char *files[], char *outputName)
 {
-    printf("Arsiv olusturuluyor...\n");
+    FILE *archive;
+
+    FILE *inputFile;
+
+    char metadata[4096] = "";
+
+    char temp[512];
+
+    char buffer[1024];
+
+    int i;
+
+    long fileSize;
+
+    struct stat fileStat;
+
+    archive = fopen(outputName, "w");
+
+    if(archive == NULL)
+    {
+        printf("Arsiv dosyasi olusturulamadi!\n");
+        return;
+    }
+
+    /* METADATA OLUSTUR */
+
+    for(i = 0; i < fileCount; i++)
+    {
+        inputFile = fopen(files[i], "r");
+
+        if(inputFile == NULL)
+        {
+            printf("%s dosyasi acilamadi!\n", files[i]);
+            fclose(archive);
+            return;
+        }
+
+        stat(files[i], &fileStat);
+
+        fseek(inputFile, 0, SEEK_END);
+
+        fileSize = ftell(inputFile);
+
+        rewind(inputFile);
+
+        sprintf(temp, "|%s,%o,%ld|",
+                files[i],
+                fileStat.st_mode & 0777,
+                fileSize);
+
+        strcat(metadata, temp);
+
+        fclose(inputFile);
+    }
+
+    /* ILK 10 BYTE = METADATA BOYUTU */
+
+    fprintf(archive, "%010ld", strlen(metadata));
+
+    /* METADATA YAZ */
+
+    fprintf(archive, "%s", metadata);
+
+    /* DOSYA ICERIKLERI */
+
+    for(i = 0; i < fileCount; i++)
+    {
+        inputFile = fopen(files[i], "r");
+
+        if(inputFile == NULL)
+        {
+            continue;
+        }
+
+        while(fgets(buffer, sizeof(buffer), inputFile))
+        {
+            fprintf(archive, "%s", buffer);
+        }
+
+        fclose(inputFile);
+    }
+
+    fclose(archive);
+
+    printf("Dosyalar birlestirildi.\n");
 }
